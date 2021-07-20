edeea10d54f8deb8e74ed9d4785c85513cfc137b
spenceryeager <36802222+spenceryeager@users.noreply.github.com>
2021-07-20 16:52:57 -0700

    initial upload
    initial upload of the file used to generate a voltammogram from a single data file

---
+++ singleVoltammogram.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import tkinter.filedialog as fd
+
+#only works for a single file
+working_file = fd.askopenfilename(initialdir="/run/user/1000/gvfs/sftp:host=file.engr.arizona.edu/Research/Ratcliff/Spencer")
+print(working_file)
+
+# opening file, and counting how many lines are present until headers
+file = open(working_file, 'r')
+count = 0
+for line in file:
+    if line.strip() == "Potential/V, Current/A":
+        row = count
+    count += 1
+
+cv = pd.read_csv(working_file, skiprows=row)
+plt.rc('font', size=15)
+plt.rc('axes', labelsize=15)
+plt.rc('xtick', labelsize=15)
+plt.rc('ytick', labelsize=15)
+plt.figure(figsize=(10,6))
+plt.plot(cv['Potential/V'], cv[' Current/A'], color='red')
+plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
+plt.xlabel('Potential (V)')
+plt.ylabel('Current (A)')
+plt.show()
+# print(cv)

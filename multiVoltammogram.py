7f922c8f5c12a4875fc4bda8d431e68120be77a0
spenceryeager <36802222+spenceryeager@users.noreply.github.com>
2021-07-21 13:07:59 -0700

    Add files via upload

--- multiVoltammogram.py
+++ multiVoltammogram.py
@@ -5,6 +5,7 @@ import tkinter.filedialog as fd
 import tkinter.simpledialog as sd
 import tkinter.messagebox as mb
 from tkinter import *
+import numpy as np
 
 root = Tk()
 root.withdraw()
@@ -12,17 +13,21 @@ root.withdraw()
 number = sd.askinteger(title="Enter value", prompt="How many voltammograms are you plotting?")
 if number == None:
     quit()
-workingdir = fd.askdirectory(initialdir="/home/spenceryeager/Documents/python_bits/voltammetry", title="Select data folder")
+workingdir = fd.askdirectory(initialdir="enter folder here", title="Select data folder")
 if workingdir == ():
     quit()
 
 
-count = 0
+
 plt.rc('font', size=15)
 plt.figure(figsize=(11,8))
 plt.xlabel('Potential (V)')
 plt.ylabel('Current (A)')
 
+# setting up multi colors based on how many voltammograms are being plotted
+colors = plt.cm.viridis(np.linspace(0, 1, number))
+
+count = 0
 while count < number:
     workingfile = fd.askopenfilename(initialdir=workingdir)
     label = sd.askstring("Enter label", "Enter a legend label")
@@ -36,11 +41,16 @@ while count < number:
         count2 += 1
 
     cv = pd.read_csv(workingfile, skiprows=row)
-    plt.plot(cv['Potential/V'], cv[' Current/A'], color='red', label=label)
+    plt.plot(cv['Potential/V'], cv[' Current/A'], color=colors[count], label=label)
     plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
     count += 1
 legendChoice = mb.askyesno("Display legend?", "Display legend?")
+titleChoice = mb.askyesno("Display title?", "Display title?")
 if legendChoice:
     plt.legend(loc='best')
+
+if titleChoice:
+    title = sd.askstring("Enter title", "Enter a title for the graph")
+    plt.title(title)
 plt.show()
 # # print(cv)

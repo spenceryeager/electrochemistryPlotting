3315b0d6bd4b6b4d65dc79e60c131e9848b6937f
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-11 16:35:39 -0700

    continuing implementation

--- plotFunctions/plotSettings.py
+++ plotFunctions/plotSettings.py
@@ -2,16 +2,28 @@ import matplotlib.pyplot as ply
 import numpy as np
 import pandas as pd
 import tkinter.messagebox as mb
+import tkinter.simpledialog as sd
 from tkinter import *
 root = Tk()
 root.withdraw()
 
 
-def makePlot(x, y):
-    plotOK = False
-    
+def sizing(dimension):
+    dim = sd.askinteger(title=("Enter "+dimension), prompt=("Enter figure " + dimension))
+    return dim
 
 
+def labels(axis):
+    axLabel = sd.askstring(title= axis + " label", prompt=("Enter " + axis + " label. LaTeX formatting can be used"))
+    return axLabel
 
-    while not plotOK:
-        plotOK = mb.askyesno(title="Keep Plot?", message="Keep the plot? (Yes to keep, No to redefine graph parameters)")
+
+def makePlot():
+    width = sizing("width")
+    height = sizing("height")
+    yax = labels("y axis")
+    xax = labels("x axis")
+    titlechoice = mb.askyesno(title="Title?", message="Make a graph title?")
+    if titlechoice:
+        title = sd.askstring(title="Title", message="Enter graph title")
+    fontsize = sd.askinteger(title="Font Size", message="Enter font size for graph")

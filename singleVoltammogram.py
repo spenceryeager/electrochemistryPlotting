bf6c2ae4f6b2c6bad2e87ac61521277dfc7af190
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-30 13:46:10 -0700

    Changed to current density

--- singleVoltammogram.py
+++ singleVoltammogram.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import tkinter.filedialog as fd
+import numpy as np
 
 #only works for a single file
-working_file = fd.askopenfilename(initialdir="enter initial directory here")
+working_file = fd.askopenfilename(initialdir="enter working directory")
 print(working_file)
 
 # opening file, and counting how many lines are present until headers
@@ -16,14 +17,16 @@ for line in file:
     count += 1
 
 cv = pd.read_csv(working_file, skiprows=row)
+j = np.array(cv[' Current/A'])
+j = j / 0.71
 plt.rc('font', size=15)
 plt.rc('axes', labelsize=15)
 plt.rc('xtick', labelsize=15)
 plt.rc('ytick', labelsize=15)
 plt.figure(figsize=(10,6))
-plt.plot(cv['Potential/V'], cv[' Current/A'], color='red')
+plt.plot(cv['Potential/V'], j, color='red')
 plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
 plt.xlabel('Potential (V)')
-plt.ylabel('Current (A)')
+plt.ylabel('Current Density (A/cm$^{2}$)')
 plt.show()
 # print(cv)

e08f995c8f1ebd4c5c8a0a2a314098885e926a9e
Spencer Yeager <yeager.spencer@gmail.com>
2021-08-31 22:46:47 -0700

    implementing file save

--- plotLoading/fileSave.py
+++ plotLoading/fileSave.py
@@ -1,1 +1,9 @@
-#FILL THIS OUT
+import os
+
+def makefile(workingdir, newdir, filename):
+    path = os.path.join(workingdir, newdir)
+    isdir = os.path.isdir(path)
+    if not isdir:
+        os.mkdir(path)
+    filepath = os.path.join(path, filename)
+    return filepath

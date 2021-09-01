cc2febb19a011c3daedf8b6c6eb989b64395f252
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-01 00:47:52 -0700

    Completed calculation section and plotting

--- mobilityPlot.py
+++ mobilityPlot.py
@@ -5,31 +5,57 @@ import tkinter.filedialog as fd
 import tkinter.simpledialog as sd
 import tkinter.messagebox as mb
 import numpy as np
-import scipy.integrate as integrate
+import scipy.constants as constant
 import os
-from plotLoading.fileSelect import select
+from plotLoading.fileSelect import *
 
 # Author: Spencer Yeager, University of Arizona, Aug 2021
 # This program goes hand in hand with the holeDensity.py and conductivityPlot.py program
 # This will create mobility plots from corresponding conductivity and and hole density plots.
 # Additionally it will save the calculated values for plotting in other software.
 
+#01-Sep-2021, this program is a bit sloppy. I hope to have time in the future to make it look a little better.
+
 root = Tk()
 root.withdraw()
 
-mb.showinfo(title="Select Data", message="Select data folder then file containing energy and DOS data")
-
+mb.showinfo(title="Select Data", message="Select data folder then file containing energy and hole data")
 workingdir, workingfile = select()
-# interval = sd.askfloat("Data Intervals", 'Enter data intervals')
+# #debugging:
+# workingfile = "/home/spenceryeager/Documents/calculations/mobility/calculated_holes.csv"
+hole = pd.read_csv(workingfile, usecols=[1, 2])
 
-# for debugging
-# workingdir = '/home/spenceryeager/Documents/python_bits/holeDensity'
-# workingfile='/home/spenceryeager/Documents/python_bits/holeDensity/test.csv'
-# interval = 0.001
+mb.showinfo(title="Select Data", message="Select data file containing conductivity data")
+workingfile = singleFileSelect(workingdir)
+#debugging:
+# workingfile = '/home/spenceryeager/Documents/calculations/mobility/clo4_calculated_conductivity.csv'
+cond = pd.read_csv(workingfile, usecols=[5, 6])
+
+
+# eventually remove this - but need to convert energy in conductivity to negative.
+cond_ev = np.array(cond['Energy wrt Vac (eV)'])
+cond_ev = cond_ev * -1
+cond_values = np.array(cond['Conductivity (S/cm)'])
+hole_ev = np.array(hole['Energy wrt Vac (eV)'])
+hole_number = np.array(hole['Hole Density (cm^-3)'])
+
+print(len(cond_ev), len(hole_ev))
+
+count = 0
+for i in cond_ev:
+    if i == hole_ev[0]:
+        position = count
+    count +=1
+
+cond_ev = cond_ev[position:]
+cond_values = cond_values[position:]
+
+mobility = cond_values / (hole_number * constant.elementary_charge)
+
+print(mobility)
 
 # file saving
-vals = {'Energy wrt Vac (eV)': dos['Energy wrt Vac (eV)'][1:], 'Hole Density (cm^-3)': hole, 'Log(Hole Density (cm^-3))':
-        holelog}
+vals = {'Energy wrt Vac (eV)': hole_ev, 'Hole Density (cm^-3)': mobility}
 calc_vals = pd.DataFrame(data=vals)
 
 
@@ -42,11 +68,11 @@ def makefile(workingdir, newdir, filename):
     return filepath
 
 
-calc_vals.to_csv(makefile(workingdir, "calculated_holes", "calculated_holes.csv"))
+calc_vals.to_csv(makefile(workingdir, "calculated_mobility", "calculated_mobility.csv"))
 
 # plot
-plt.plot(dos['Energy wrt Vac (eV)'][1:], holelog)
-plt.xlim(max(dos['Energy wrt Vac (eV)'][1:]), min(dos['Energy wrt Vac (eV)'][1:]))
+plt.plot(hole_ev, mobility)
+plt.xlim(max(hole_ev), min(hole_ev))
 plt.xlabel("Energy w.r.t Vacuum (eV)")
-plt.ylabel(r"log Hole Density (cm$^{-3}$)")
+plt.ylabel(r"Mobility (cm$^{2}$ V$^{-1}$ s$^{-1}$)")
 plt.show()

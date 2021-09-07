f3168ed3254bf46d9e7010a4229b5d845059f949
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-06 19:21:20 -0700

    reference fixes

--- singleVoltammogram.py
+++ singleVoltammogram.py
@@ -5,24 +5,28 @@ rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Arial']
 rcParams['font.weight'] = 'bold'
 rcParams['axes.labelweight'] = 'bold'
-rcParams['figure.dpi'] = 300
 rcParams['savefig.dpi'] = 300
 import os
 import tkinter.filedialog as fd
+import tkinter.simpledialog as sd
+from tkinter import *
 import numpy as np
+root = Tk()
+root.withdraw()
 
 
 def main():
     #only works for a single file
-    working_file = fd.askopenfilename(initialdir="enter working directory")
-    cv = pd.read_csv(working_file, skiprows=rowskip(workingfile))
+    filepath = sd.askstring(title="Enter directory path", prompt="Enter the path to the directory containing file of interest")
+    working_file = fd.askopenfilename(title="Select file for plotting", initialdir=filepath)
+    cv = pd.read_csv(working_file, skiprows=rowskip(working_file))
     j = np.array(cv[' Current/A'])
     j = j / 0.71
     V = np.array(cv['Potential/V'])
     plot(V, j)
 
 # opening file, and counting how many lines are present until headers
-def rowskip(workingfile):
+def rowskip(working_file):
     file = open(working_file, 'r')
     count = 0
     for line in file:
@@ -34,8 +38,8 @@ def rowskip(workingfile):
 
 def plot(potential, current):
     plt.rc('font', size=12)
-    plt.plot(cv['Potential/V'], j, color='red')
-    plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
+    plt.plot(potential, current, color='red', linewidth=3)
+    plt.xlim(max(potential + 0.1), min(potential - 0.1))
     plt.xlabel('Potential (V)')
     plt.ylabel('Current Density (A/cm$^{2}$)')
     plt.show()

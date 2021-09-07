3411e781f93753a380003d5fb44e1ae00b1d1d4f
Spencer Yeager <yeager.spencer@gmail.com>
2021-09-06 19:06:15 -0700

    code cleanup!

--- singleVoltammogram.py
+++ singleVoltammogram.py
@@ -1,32 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Arial']
+rcParams['font.weight'] = 'bold'
+rcParams['axes.labelweight'] = 'bold'
+rcParams['figure.dpi'] = 300
+rcParams['savefig.dpi'] = 300
 import os
 import tkinter.filedialog as fd
 import numpy as np
 
-#only works for a single file
-working_file = fd.askopenfilename(initialdir="enter working directory")
-print(working_file)
+
+def main():
+    #only works for a single file
+    working_file = fd.askopenfilename(initialdir="enter working directory")
+    cv = pd.read_csv(working_file, skiprows=rowskip(workingfile))
+    j = np.array(cv[' Current/A'])
+    j = j / 0.71
+    V = np.array(cv['Potential/V'])
+    plot(V, j)
 
 # opening file, and counting how many lines are present until headers
-file = open(working_file, 'r')
-count = 0
-for line in file:
-    if line.strip() == "Potential/V, Current/A":
-        row = count
-    count += 1
+def rowskip(workingfile):
+    file = open(working_file, 'r')
+    count = 0
+    for line in file:
+        if line.strip() == "Potential/V, Current/A":
+            row = count
+        count += 1
+    return row
 
-cv = pd.read_csv(working_file, skiprows=row)
-j = np.array(cv[' Current/A'])
-j = j / 0.71
-plt.rc('font', size=15)
-plt.rc('axes', labelsize=15)
-plt.rc('xtick', labelsize=15)
-plt.rc('ytick', labelsize=15)
-plt.figure(figsize=(10,6))
-plt.plot(cv['Potential/V'], j, color='red')
-plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
-plt.xlabel('Potential (V)')
-plt.ylabel('Current Density (A/cm$^{2}$)')
-plt.show()
-# print(cv)
+
+def plot(potential, current):
+    plt.rc('font', size=12)
+    plt.plot(cv['Potential/V'], j, color='red')
+    plt.xlim(max(cv['Potential/V'] + 0.1), min(cv['Potential/V'] - 0.1))
+    plt.xlabel('Potential (V)')
+    plt.ylabel('Current Density (A/cm$^{2}$)')
+    plt.show()
+    # print(cv)
+
+if __name__ == '__main__':
+    main()

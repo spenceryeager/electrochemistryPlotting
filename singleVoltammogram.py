52b193b7f47574ad7f007c9f0680f895536fe414
Spencer Yeager <yeager.spencer@gmail.com>
2023-08-16 13:07:39 -0700

    updated to reflect group figure style

--- singleVoltammogram.py
+++ singleVoltammogram.py
@@ -5,6 +5,7 @@ import tkinter.filedialog as fd
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib import rcParams
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Arial']
@@ -17,19 +18,24 @@ root.withdraw()
 
 def main():
     # only works for a single file
-    filepath = sd.askstring(title="Enter directory path",
-                            prompt="Enter the path to the directory containing file of interest")
-    working_file = fd.askopenfilename(
-        title="Select file for plotting", initialdir=filepath)
+    # filepath = sd.askstring(title="Enter directory path",
+    #                         prompt="Enter the path to the directory containing file of interest")
+    # working_file = fd.askopenfilename(
+    #     title="Select file for plotting", initialdir=filepath)
+    electrode_area = 0.71 #cm2
+    working_file = r"R:\Spencer Yeager\data\NiOx_Project\2023\08_Aug\14Aug2023_iodide-echem-nonaqu\10mM_imidazolium_iodide_run1.csv"
+    save_fig_filepath= r"R:\Spencer Yeager\group_meetings\general_group_meetings\2023\16Aug2023"
+    save_fig = os.path.join(save_fig_filepath, "IodideNonaqu_AgCl-Ref.svg")
     cv = pd.read_csv(working_file, skiprows=rowskip(working_file))
     if len(cv.columns) == 2:
         j = np.array(cv[' Current/A'])
-        j = j / 0.71
+        j = j / electrode_area
+        j = j * (10**6)
 
     if len(cv.columns) == 3:
         j = np.array(cv[' i2/A'])
     V = np.array(cv['Potential/V'])
-    plot(V, j)
+    plot(V, j, save_fig)
 
 
 # def column_comp(comp_val, col_list):
@@ -49,13 +55,21 @@ def rowskip(working_file):
     return row
 
 
-def plot(potential, current):
+def plot(potential, current, save_fig):
+    fontsize = 40
+    mpl.rcParams.update({'font.size': fontsize, 'figure.autolayout': True})
+    fig, ax = plt.subplots(figsize=(14,10), tight_layout=True)
     plt.rc('font', size=12)
-    plt.figure(figsize=(10,6))
-    plt.plot(potential, current, color='red', linewidth=1)
-    plt.xlim(max(potential + 0.1), min(potential - 0.1))
-    plt.xlabel('Potential (V)')
-    plt.ylabel('Current Density (A/cm$^{2}$)')
+    ax.plot(potential, current, color='red', linewidth=7)
+    ax.set_xlim(max(potential + 0.1), min(potential - 0.1))
+    ax.set_xlabel('Potential (V) vs. Ag/AgCl')
+    ax.set_ylabel('Current Density ($\mu$A/cm$^{2}$)')
+    ax.xaxis.labelpad = 5
+    ax.yaxis.labelpad = 5
+    ax.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(3)
+    # plt.savefig(save_fig)
     plt.show()
     # print(cv)
 

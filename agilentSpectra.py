5b2b5ac71bfaf47842b92972b1708ad39beaf7e0
Spencer Yeager <yeager.spencer@gmail.com>
2023-10-16 12:48:23 -0700

    agilent text file plotting

--- agilentSpectra.py
+++ agilentSpectra.py
@@ -1,9 +1,8 @@
-from tkinter import *
-import tkinter.simpledialog as sd
+from matplotlib import cm
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 import os
 import numpy as np
 import pandas as pd
-from plotLoading.fileSelect import *
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 rcParams['font.family'] = 'sans-serif'
@@ -11,71 +10,39 @@ rcParams['font.sans-serif'] = ['Arial']
 rcParams['font.weight'] = 'bold'
 rcParams['axes.labelweight'] = 'bold'
 rcParams['savefig.dpi'] = 300
-root = Tk()
-root.withdraw()
-
 
 def main():
-    workingdir = workingdirSelect()
-    filename = singleFileSelect(workingdir)
-    workingfile = os.path.join(workingdir, filename)
-    data = dataLoading(workingfile)
-    label_list = list(data)
-    label_list = label_list[1:]
-    plotting(label_list, data, colorlist(len(list(data))-1))
+    file = r"R:\Spencer Yeager\data\SPECS-Project\2023\16Oct2023_100kgP3HT_NewMat\SpecEchemCV\CV1.TXT"
+    specechem_data = pd.read_csv(file, skiprows=5, sep='\t')
+    print(specechem_data.head())
+    fig, ax = plt.subplots(figsize=(14,10), tight_layout=True)
+    data_range = np.arange(1, 30, 1)
+    voltages = np.linspace(-0.2, 1.4, len(data_range))
+    colormap = cm.get_cmap('Reds')
+    color_range = colormap(np.linspace(0, 1, 30))
+    print(color_range)
 
 
-def dataLoading(workingfile):
-    data = pd.read_csv(workingfile)
-    data = data.iloc[:, :-1]
-    column = column_name()
-    data.columns = column
-    print(data)
-    return data
+    for i in data_range:
+        ax.plot(specechem_data.iloc[:,0], specechem_data.iloc[:,i], color=color_range[i], linewidth=7)
+        
+
+    ax.set_xlim(350, 1100)
+    ax.set_xlabel('Wavelength (nm)', fontweight='bold')
+    ax.set_ylabel('Differential Absorbance (arb. units)', fontweight='bold')
+    sm = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=-0.2, vmax=1.4))
+    cbar = plt.colorbar(sm)
+    cbar.set_label('Potential (V) versus Ag/Ag$^{+}$', rotation=270, fontweight='bold', labelpad=20)
+    cbar.outline.set_linewidth(3)
+    ax.xaxis.labelpad = 5
+    ax.yaxis.labelpad = 5
+    ax.tick_params(axis = 'both', direction='in', which='both', length=18, width=3)
+    
+
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(3)
 
 
-def column_name():
-    columnlist = ['Wavelength']
-    init_V = -0.4
-    final_V = 0.8
-    delta_V = final_V - init_V
-    sr = 0.1 # V/s
-    value = init_V +0.1
-    while value < final_V:
-        value = np.add(value, 0.1)
-        value = np.round(value, 1)
-        columnlist.append(str(value)+"V")
-    return columnlist
-
-
-def colorlist(data_points):
-    # quick note: many colormaps in mpl have really light colors not good for plots.
-    # so I will double the amount of color points,
-    # then create a colormap list starting at the darker colors only.
-    colors = plt.cm.BrBG(np.linspace(0, 1, data_points*2))
-    colors = colors[data_points:]
-    return colors
-
-
-def plotting(label_list, data, colors):
-    plot_number = len(label_list)
-    count = 0
-    for i in data:
-        if i != 'Wavelength':
-            plt.plot(data['Wavelength'], data[i], color=colors[count], label=i, linewidth=2)
-            print(i)
-            count += 1
-    # wavelength = data[0]
-    # while count < plot_number:
-    #     data = datalist[count]
-    #     plt.plot(data['Wavelength (nm)'], data['Absorbance (AU)'],
-    #              color=colors[count], linewidth=3, label=label_list[count])
-    #     count += 1
-    plt.xlim(330, 1000)
-    plt.legend(loc='best', prop={'size':6})
-    plt.xlabel("Wavelength (nm)")
-    plt.ylabel("Absorbance (arb. units)")
-    plt.ylim((-0.25, 0.2))
     plt.show()
 
 

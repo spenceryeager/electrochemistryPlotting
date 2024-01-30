a7be6fdc610305a141ff85b8eefd92fe3fb9ad09
Spencer Yeager <yeager.spencer@gmail.com>
2024-01-29 17:33:36 -0700

    Adding fermi function get

--- marcus_gericher_polymer_DOS.py
+++ marcus_gericher_polymer_DOS.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
+# Note: This approximation is coming from this Nature Comms paper:
+# https://www.nature.com/articles/s41467-017-01264-2
+# Equation 5 specifically. 
 
 def main():
     print('hello')
 
+
+def get_fermi():
+    # this will be used to get the Fermi level by approximating the location where half the states would be occupied.
+    print()
 
 if __name__ == "__main__":
     main()

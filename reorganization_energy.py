fa34afbd0c27618f274ef88c5715e4201a7a54a9
Spencer Yeager <yeager.spencer@gmail.com>
2024-02-07 15:38:45 -0700

    added a bit of extra stuff for outersphere reorganization energy. Tired now

--- reorganization_energy.py
+++ reorganization_energy.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
+import scipy.constants as constants
 
 
 def main():
@@ -9,15 +10,19 @@ def main():
 
 
 def reorganization_energy():
-    inner_sphere()
+    outer_sphere()
 
 
 def inner_sphere():
     print('hi')
 
 
-def outer_sphere():
-    print('hi')
+def outer_sphere():  # this component is taken from this publication: https://doi.org/10.1016/0022-0728(89)85030-2
+    avogadro = constants.physical_constants['Avogadro constant'][0] # molecules / mol
+    # print(avogadro)
+    e0 = constants.epsilon_0 # F m^-1
+    print(e0)
+    
 
 
 if __name__ == "__main__":

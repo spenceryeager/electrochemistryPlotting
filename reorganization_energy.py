c2fbe1172f1e1265e373577c0089c9bf89dc3700
Spencer Yeager <yeager.spencer@gmail.com>
2024-02-07 15:29:24 -0700

    added framework for reorganization energy calculation

---
+++ reorganization_energy.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+
+def main():
+    reorganization_energy()
+
+
+def reorganization_energy():
+    inner_sphere()
+
+
+def inner_sphere():
+    print('hi')
+
+
+def outer_sphere():
+    print('hi')
+
+
+if __name__ == "__main__":
+    main()

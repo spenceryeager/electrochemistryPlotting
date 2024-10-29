14efdcbba29740f3b18474dc60ce2b3132568fc8
Spencer Yeager <yeager.spencer@gmail.com>
2024-10-29 10:41:40 -0400

    need to take a closer look at what's happening here.

--- marcus_theory_forward_backward.py
+++ marcus_theory_forward_backward.py
@@ -17,12 +17,23 @@ def main():
     formal_potential = -5.05 # eV. Of Ferrocene, from literature.
     temperature = 298 # K
     ef = fermi_level_polymer(polymer_dos_load)
-    preintegral = preintergral_term(polymer_dos_load['Energy wrt Vac (eV)'], polymer_dos_load['DOS (states/(eV cm^3))'], reorganization_energy, formal_potential, temperature, ef)
-    integral_term = integrate.cumulative_trapezoid(preintegral, dx=0.001)
-    integral_term = integral_term * (1.6*10**-22)
-    print(integral_term)
+
+    # Calculation
+    kf = kf_preintegral(polymer_dos_load['Energy wrt Vac (eV)'], polymer_dos_load['DOS (states/(eV cm^3))'], reorganization_energy, formal_potential, temperature, ef)
+    kf = integrate.cumulative_trapezoid(kf, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
+    kf = kf * (1.6*10**-22) # time constant
+
+    kb = kb_preintegral(polymer_dos_load['Energy wrt Vac (eV)'], polymer_dos_load['DOS (states/(eV cm^3))'], reorganization_energy, formal_potential, temperature, ef)
+    kb = integrate.cumulative_trapezoid(kb, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
+    kb = kb * (1.6*10**-22) # time constant
+
+    # print(integral_term)
     fig, ax = plt.subplots()
-    ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], integral_term)
+    # ax.plot(polymer_dos_load['DOS (states/(eV cm^3))'], polymer_dos_load['Energy wrt Vac (eV)'])
+
+    ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], kf)
+    ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], kb)
+
     plt.show()
     # final_index = int(len(polymer_dos_load))
     
@@ -32,24 +43,32 @@ def main():
   
 
 
-def preintergral_term(polymer_energy, polymer_dos, reorganization_energy, formal_potential, temperature, ef):
+def kf_preintegral(polymer_energy, polymer_dos, reorganization_energy, formal_potential, temperature, ef):
     term1 = polymer_dos
     # print(term1)
     term2 = (1 - fermi_dirac_distribution(polymer_energy, ef, temperature))
     # print(term2)
     term3_num = np.negative(polymer_energy - (2.718 * formal_potential) - reorganization_energy) ** 2
     term3_denom = 4 * reorganization_energy * constants.physical_constants['Boltzmann constant in eV/K'][0] * temperature
-    # print(term3_denom)
-    # term3 = np.exp((-(polymer_energy - (2.718 * formal_potential) - reorganization_energy)**2) * np.reciprocal(4 * reorganization_energy * constants.physical_constants['Boltzmann constant in eV/K'][0] * temperature))
-    # print(term3_num)
     term3 = term3_num / term3_denom
-    # print(term3)
     return term1 * term2 * term3
 
 
+def kb_preintegral(polymer_energy, polymer_dos, reorganization_energy, formal_potential, temperature, ef):
+    term1 = polymer_dos
+    # print(term1)
+    term2 = (fermi_dirac_distribution(polymer_energy, ef, temperature))
+    # print(term2)
+    term3_num = np.negative(polymer_energy - (2.718 * formal_potential) - reorganization_energy) ** 2
+    term3_denom = 4 * reorganization_energy * constants.physical_constants['Boltzmann constant in eV/K'][0] * temperature
+    term3 = term3_num / term3_denom
+    return term1 * term2 * term3
+
 
 def fermi_dirac_distribution(e, ef, temperature):
-    fermi = 1 + np.exp((e - ef) * np.reciprocal(temperature * constants.physical_constants['Boltzmann constant in eV/K'][0]))
+    numerator = (e - ef)
+    denominator = temperature * constants.physical_constants['Boltzmann constant in eV/K'][0]
+    fermi = 1 + np.exp(numerator / denominator)
     return np.reciprocal(fermi)
 
 

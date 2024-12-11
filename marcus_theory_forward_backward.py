3ff4b0f86c9b07e1677bb75a28fff660daa9dd99
Spencer Yeager <yeager.spencer@gmail.com>
2024-12-10 21:57:14 -0700

    Gotta clean this up, but can't do that right now

--- marcus_theory_forward_backward.py
+++ marcus_theory_forward_backward.py
@@ -11,133 +11,91 @@ import scipy.integrate as integrate
 
 def main():
     # Constants!
-    polymer_dos = r"E:\RDrive_Backup\Spencer Yeager\papers\paper4_pbtttt_p3ht_transfer_kinetics\worked-up-data\calculated_DOS\p3ht_calculated_DOS.csv"
+    polymer_dos = r"E:\RDrive_Backup\Spencer Yeager\papers\paper4_pbtttt_p3ht_transfer_kinetics\worked-up-data\calculated_DOS\p3ht_oxidation_DOS.csv"
+    polymer_red_dos = r"E:\RDrive_Backup\Spencer Yeager\papers\paper4_pbtttt_p3ht_transfer_kinetics\worked-up-data\calculated_DOS\p3ht_reduction_DOS.csv"
     polymer_dos_load = pd.read_csv(polymer_dos)
+    polymyer_dos_red_load = pd.read_csv(polymer_red_dos)
     reorganization_energy = 0.39 # eV. Calculated from reorganization_energy.py
     formal_potential = -5.05 # eV. Of Ferrocene, from literature.
     temperature = 298 # K
-    conc_red = 0.003 # molar
+    conc_red = 0.003 # concentration of reductant in electrolyte. Molar
+    conc_ox = 0.003 # concentration of oxidant in electrolyte. Molar
 
-    ef = fermi_level_polymer(polymer_dos_load)
-    # fermi_dirac = fermi_dirac_distribution(polymer_dos_load['Energy wrt Vac (eV)'], ef, temperature)
+    # Fermi Level and Fermi Dirac function section
+    ef = fermi_level_polymer(polymer_dos_load) # approximating the Fermi level of the polymer from DOS(E)
+    ef_red = fermi_level_polymer(polymyer_dos_red_load)
+    print(ef_red)
+
+    # Fermi stuff for the Kf
+    fermi_dirac = fermi_dirac_distribution(polymer_dos_load['Energy wrt Vac (eV)'], ef, temperature) 
+    subtracted_fermi = (1 - fermi_dirac) # Use this one for determining the kf
+
+    # Fermi stuff for the Kb
+    fermi_dirac_kb = fermi_dirac_distribution(polymyer_dos_red_load['Energy wrt Vac (eV)'], ef_red, temperature) 
 
 
-    fc_dos = dos_estimate_red(reorganization_energy, conc_red, temperature, formal_potential, polymer_dos_load['Energy wrt Vac (eV)'])
-    # print(fc_dos)
-    # print(polymer_dos_load['Energy wrt Vac (eV)'])
+    # Electrolyte DOS section
+    electrolyte_dos_ox = dos_estimate_ox(reorganization_energy, conc_ox, temperature, formal_potential, polymer_dos_load['Energy wrt Vac (eV)']) # Use this one for determining kf. Oxidant is species being REDUCED.
 
+    electrolyte_dos_red = dos_estimate_red(reorganization_energy, conc_red, temperature, formal_potential, polymer_dos_load['Energy wrt Vac (eV)']) # Use this one for determining the kf. Gerischer language is sort of inversed - this is the REDUCTANT DOS(E), i.e. the species that gets oxidized
 
-    combined_dos = polymer_dos_load['DOS (states/(eV cm^3))'] * fc_dos 
-    kf = kf_preintegral(polymer_dos_load['Energy wrt Vac (eV)'], combined_dos, reorganization_energy, formal_potential, temperature, ef)
-    kf = integrate.cumulative_trapezoid(kf, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
+    # Kb section
+    polymyer_dos_red_load = polymyer_dos_red_load.reindex(index = polymyer_dos_red_load.index[::-1]).reset_index(drop=True)
+    combined_kb_terms = polymyer_dos_red_load['DOS (states/(eV cm^3))'] * electrolyte_dos_ox * fermi_dirac_kb
+    kb = integrate.cumulative_trapezoid(combined_kb_terms, polymer_dos_load['Energy wrt Vac (eV)'])
+    kb = kb * (1.6*10**-22) # time constant
+    kb = np.abs(kb)
+
+    # Kf section
+    # Multiplying the polymer DOS(E), electrolyte DOS(E) and the Fermi Dirac distribution for kf
+    combined_kf_terms = polymer_dos_load['DOS (states/(eV cm^3))'] * electrolyte_dos_red * subtracted_fermi
+    kf = integrate.cumulative_trapezoid(combined_kf_terms, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
     kf = kf * (1.6*10**-22) # time constant
+    kf = np.abs(kf)
 
     fig, ax = plt.subplots()
+    ax.plot(polymyer_dos_red_load['Energy wrt Vac (eV)'][1:], kb)
     ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], kf)
     ax.set_yscale('log')
     plt.show()
 
-
-
-    # fc_dos = dos_estimate_red(reorganization_energy, conc_red, temperature, formal_potential, polymer_dos_load['Energy wrt Vac (eV)'])
-    # print(fc_dos)
-    # print(polymer_dos_load['Energy wrt Vac (eV)'])
-    
-
-    # combined_dos = polymer_dos_load['DOS (states/(eV cm^3))'] * fc_dos
-    # print('combined_dos')
-    # print(combined_dos)
-
-    # integrated_dos = integrate.cumulative_trapezoid(combined_dos, np.negative(polymer_dos_load['Energy wrt Vac (eV)']))
-    # print('integrated_dos')
-    # print(integrated_dos)
-
-    # kox = integrated_dos / ((1.6 * 10**-19) * (6 * 10**17))
-    
-    # print('kox')
-    # print(kox)
-    # fig, ax = plt.subplots()
-    # ax.plot(polymer_dos_load['Energy wrt Vac (eV)'], (fc_dos))
-    # # plt.show()
-    # # ax.set_yscale('log')
-    # plt.show()
-
-
-    # # Calculation
-    # kf = kf_preintegral(polymer_dos_load['Energy wrt Vac (eV)'], polymer_dos_load['DOS (states/(eV cm^3))'], reorganization_energy, formal_potential, temperature, ef)
-    # kf = integrate.cumulative_trapezoid(kf, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
-    # kf = kf * (1.6*10**-22) # time constant
-
-    # # kb = kb_preintegral(polymer_dos_load['Energy wrt Vac (eV)'], polymer_dos_load['DOS (states/(eV cm^3))'], reorganization_energy, formal_potential, temperature, ef)
-    # # kb = integrate.cumulative_trapezoid(kb, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
-    # # kb = kb * (1.6*10**-22) # time constant
-
-    # print(integral_term)
-    # fig, ax = plt.subplots()
-    # # ax.plot(polymer_dos_load['DOS (states/(eV cm^3))'], polymer_dos_load['Energy wrt Vac (eV)'])
-
-    # ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], np.abs(kf))
-    # # ax.plot(polymer_dos_load['Energy wrt Vac (eV)'][1:], kb)
-
-    # ax.set_yscale('log')
-
-    # plt.show()
-    # final_index = int(len(polymer_dos_load))
-    
-
-    # kfs = integrate.cumulative_trapezoid(kf, x=polymer_dos_load['Energy wrt Vac (eV)'], dx=0.001)
-    # print(kfs)
   
 
 def dos_estimate_red(reorg_energy, concentration, temperature, eo, e):
+
     k = constants.physical_constants['Boltzmann constant in eV/K'][0] # boltzmann
     electron = constants.e # electron charge
-    # term1 = concentration
-    # term2 = np.reciprocal(np.sqrt(4 * reorg_energy * k * temperature))
-    term1 = 1
-    term2 = 1
-    term3_num = (e -  eo - reorg_energy) ** 2
+    # term1 = concentration # We do not need to incorporate this into the formulation. It is canceled out ultimately.
+    # term2 = np.reciprocal(np.sqrt(4 * reorg_energy * k * temperature)) # We do not need to incorporate this into the formulation. It is a normalization parameter
+    term3_num = (e -  eo + reorg_energy) ** 2
     term3_denom = (4 * k * reorg_energy * temperature)
     term3_division = np.divide(term3_num, term3_denom)
     term3 = np.exp(-term3_division)
-    dos_red = term1 * term2 * term3
-    dos_red = dos_red * (6.02 * 10**23) /1000
-
+    dos_red = term3
     return dos_red
 
 
 def dos_estimate_ox(reorg_energy, concentration, temperature, eo, e):
     k = constants.physical_constants['Boltzmann constant in eV/K'][0] # boltzmann
     electron = constants.e # electron charge
-    term1 = concentration
-    term2 = np.reciprocal(np.sqrt(4 * reorg_energy * k * temperature))
-    # term1 = 1
-    # term2 = 1
-    term3_num = (e -  eo + reorg_energy) ** 2
+
+    term3_num = (e -  eo - reorg_energy) ** 2
     term3_denom = (4 * k * reorg_energy * temperature)
     term3_division = np.divide(term3_num, term3_denom)
     term3 = np.exp(-term3_division)
-    dos_ox = term1 * term2 * term3
-    dos_ox = dos_ox / (6.02 * 10**23) / 1000
-    print(dos_ox)
+    dos_ox = term3
     return dos_ox
 
 
 def kf_preintegral(polymer_energy, polymer_dos, reorganization_energy, formal_potential, temperature, ef):
     term1 = polymer_dos
-    # print(term1)
     term2 = (1 - fermi_dirac_distribution(polymer_energy, ef, temperature))
-    # print(term2)
-    term3_num = np.negative((polymer_energy - (2.718 * formal_potential) - reorganization_energy) ** 2)
-    print('Term 3 numerator')
-    print(term3_num)
+    term3_num = np.negative((polymer_energy - (2.718 * formal_potential) + reorganization_energy) ** 2)
     term3_denom = 4 * reorganization_energy * constants.physical_constants['Boltzmann constant in eV/K'][0] * temperature
-    print('Term 3 Denominator')
-    print(term3_denom)
     term3 = np.divide(term3_num, term3_denom)
-    # term3 = np.exp(term3)
-    print('Term 3')
-    print(term3)
+    term3 = np.exp(-term3)
+    # print('Term 3')
+    # print(term3)
     return term1 * term2 * term3
 
 
@@ -171,7 +129,6 @@ def fermi_level_polymer(polymer_dos_df):
         calculated_fermi_level_integral = np.trapz(polymer_dos_df['DOS (states/(eV cm^3))'][0:index], x=polymer_dos_df['Energy wrt Vac (eV)'][0:index], dx = 0.001)
         index+=1
 
-    
     
     return (polymer_dos_df['Energy wrt Vac (eV)'].iloc[index])
 

691bee9d6500369cc2ea3a9d349ec2de7903662f
Spencer Yeager <yeager.spencer@gmail.com>
2025-07-16 16:13:43 -0400

    Updated conductivity

--- conductivityPlot.py
+++ conductivityPlot.py
@@ -1,34 +1,66 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import tkinter.filedialog as fd
-import tkinter.simpledialog as sd
-import tkinter.messagebox as mb
-from tkinter import *
-from plotLoading import fileSelect
+import scipy.stats as stats
+# import tkinter.filedialog as fd
+# import tkinter.simpledialog as sd
+# import tkinter.messagebox as mb
+# from tkinter import *
+# from plotLoading import fileSelect
 import os
 
 
 # Author: Spencer Yeager, University of Arizona, Aug 2021
-# Use this program to generate a conductivity plot from measured current/voltage data.
+# Updated July 2025
+# Use this program to generate a conductivity plot from measured current/voltage data from a CH Instruments 
 # To understand what is required in this program, refer to the wikipedia page on the relationship between
 # current and conductivity:
 # https://en.wikipedia.org/wiki/Electrical_resistivity_and_conductivity
 
-root = Tk()
-root.withdraw()
 
-workingdir, workingfile = fileSelect.select()
+def main():
+    #########################
+    #### FILL THIS OUT ######
+    workingfile = r"D:\10July2025_insitu_conductivity_P3HT_PBTTT\p3ht\run1.csv"
+    savedir = r"D:\10July2025_insitu_conductivity_P3HT_PBTTT\p3ht\worked_up"
+    savename = r"p3ht_conductivity"
+    
+    thickness = (27 * (10 **-7)) # cm, film thickness
+    width = 3.0 # cm, part of Ossila substrates
+    area = thickness * width
+    length = 0.005 # cm, 50 um to cm, part of Ossila substrates
 
-highV = sd.askfloat(title='High Potential', prompt="Enter the high potential set")
-if highV == None:
-    fileSelect.programquit()
-potential_diff = sd.askfloat(title='Potential difference', prompt='Enter the potential difference across channel in V')
-if potential_diff == None:
-    fileSelect.programquit()
+    #########################
+    #########################
+
+    data = fileloading(workingfile) # loading data
+    conductivity_df = conductivity_calculation(data, area, length) # calculating conductivity
+    conductivity_df.to_csv(os.path.join(savedir,savename+".csv"))
+    fig, ax = plt.subplots()
+    ax.plot(conductivity_df['Potential/V'], conductivity_df['Conductivity (S/cm)'])
+    plt.show()
 
 
-def rowskip(file):  # cleans up all the extra stuff in the header
+def fileloading(workingfile):
+    data_load = pd.read_csv(workingfile, skiprows=rowskip(workingfile))
+    maximum_potential = data_load["Potential/V"].max()
+    maximum_potential_loc = data_load["Potential/V"].idxmax()
+
+    minimum_potential = data_load["Potential/V"].min()
+    minimum_potential_loc = data_load['Potential/V'].idxmin()
+
+
+    if minimum_potential_loc == 0:
+        # this is a simple way to determine if the sample set is p-type or n-type. Minimum potential location should not be the starting potential
+        data_subset = data_load[0:maximum_potential_loc+1]
+    else:
+        data_subset = data_load[0:minimum_potential_loc+1]
+
+
+    return data_subset
+
+
+def rowskip(workingfile):  # cleans up all the extra stuff in the header
     file = open(workingfile, 'r')
     count1 = 0
     for line in file:
@@ -38,65 +70,46 @@ def rowskip(file):  # cleans up all the extra stuff in the header
     return row
 
 
-rawdata = pd.read_csv(workingfile, skiprows=rowskip(workingfile))
+
+def conductivity_calculation(data, area, length):
+    potentials = [np.nan, np.nan]
+    slope = [np.nan, np.nan]
+    intercept = [np.nan, np.nan]
+    rsq = [np.nan, np.nan]
+    final_index = data.index[-1]
+    lower_index = 0
+    upper_index = 2
 
 
-def highloc(voltage, highV):
-    count = 0
-    for i in voltage:
-        if i == highV:
-            loc = count
-        count += 1
-    return loc
+    while upper_index <= final_index: # I am taking three values and doing a rolling linear fit to get the slope.
+        data_slice = data.loc[lower_index:upper_index]
+        fit = get_slope(data_slice)
+        slope.append(fit[0])
+        intercept.append(fit[1])
+        rsq.append(np.square(fit[2]))
+        lower_index += 1
+        upper_index += 1
 
 
-highPotentialLoc = highloc(rawdata['Potential/V'], highV)
-# These following two arrays are where most of the arithmetic will be performed
-potential_array = np.array(rawdata['Potential/V'][0:highPotentialLoc])
-current_array = np.array(rawdata[' i2/A'][0:highPotentialLoc])
+    data['Slope'] = slope
+    data['Intercept'] = intercept
+    data['R Squared'] = rsq
 
-# area is defined as the film thickness * channel width. Film thickness is approximated to be 27 nm
-# and length is the total combined width of all channels, which is about 3cm.
-# substrate parameters found here: https://www.ossila.com/products/interdigitated-ito-ofet-substrates
-# we use 50 um channel length substrates
+    slope_vals = data['Slope']
+    resistivity = (slope_vals * area) / length
+    conductivity = np.reciprocal(resistivity)
+    data['Resisitivity (ohm cm)'] = resistivity
+    data['Conductivity (S/cm)'] = conductivity
+    return data
 
-# spelling these calculations out to really see how it is derived.
-thickness = (27 * (10 **-7)) # cm
-width = 3.0 # cm
-area = thickness * width
-length = 0.005 # cm, 50 um to cm.
 
-resistance_array = potential_diff / current_array
-resistivity_array = (resistance_array * area) / length
-conductivity_array = resistivity_array ** -1
-ev_array = potential_array + 4.87
+def get_slope(linear_region):
+    try:
+        fit = stats.linregress(linear_region[' i2/A'], linear_region['Potential/V'])
+    except ValueError:
+        fit = [np.inf, np.nan, np.nan]
+    return fit
 
-# saving new arrays
-# making dictionary of all parameters
-allparams = {"Potential (V)": potential_array, "Current (A)": current_array, "Resistance (ohm)": resistance_array,
-             "Resistivity (ohm cm)": resistivity_array, "Conductivity (S/cm)": conductivity_array,
-             "Energy wrt Vac (eV)": ev_array}
-calc_vals = pd.DataFrame(data=allparams)
 
-def makefile(workingdir, newdir, filename):
-    path = os.path.join(workingdir, newdir)
-    isdir = os.path.isdir(path)
-    if not isdir:
-        os.mkdir(path)
-    filepath = os.path.join(path, filename)
-    return filepath
-
-calc_vals.to_csv(makefile(workingdir, "calculated_conductivity", "calculated_conductivity.csv"))
-
-# plotting
-plt.plot(ev_array, conductivity_array, color='red')
-
-titlechoice = mb.askyesno(title="Enter title?", message="Add title?")
-title = ''
-if titlechoice:
-    title = sd.askstring(title='Enter title', prompt="Enter graph title")
-plt.title(title)
-
-plt.xlabel("Energy vs. Vacuum")
-plt.ylabel("Conductivity (S/cm)")
-plt.show()
+if __name__ == "__main__":
+    main()
